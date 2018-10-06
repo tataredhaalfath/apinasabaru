b495fe3eb97fd972bd33bad7c05bfd09ccbc1519
tataredhaalfath <43898482+tataredhaalfath@users.noreply.github.com>
2018-10-06 10:35:18 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=XziZt3bFNiwvrPRif6Rnh69keOaqeqXQBgTLAjGg";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=a59VMBCUw9GwNSnzDmPtYUZJZVVwlVcdJrr92l2Y";
         
         $("#loadImage").click(function(){
             $.ajax({
